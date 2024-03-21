f4b22bf637a6e389577a26fb94a7fc5bdde86d83
unknown <crtaylorlovesstl@gmail.com>
2024-03-21 16:47:14 -0500

    react3

---
+++ part2/part3/exercises/src/components/Introduction.jsx
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction () {
+   return(
+      <div>
+         <h2>3 Fun Facts About Woodworking</h2>
+         <ol>
+            <li>Fun.</li>
+            <li>Cool.</li>
+            <li>Neat.</li>
+         </ol>
+      </div>
+   );
+}
